738c1997917ad1aee0fbc679bf4e7e5ca2f9a973
Ayoosh <>
2025-05-15 14:38:20 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+
+let root = document.getElementById("root")
+
+let element = (
+     <div>
+    <h1 className="welcome pt-2 container text-center" >Aisha Alhuraish</h1>
+    <hr class="container"></hr>
+    <div>
+        <ul class="list-unstyled container d-flex justify-content-evenly">
+            <li><img class="gap-2" src="contact.png"></img> +966 530 70 6666</li>
+            <li><img class="gap-2" src="email.png"></img> Aisha.a.f@outlook.com</li>
+            <li><img class="gap-2" src="linkedin.png"></img><a
+                    href="https://www.linkedin.com/in/aisha-alhurais?original_referer="> Aisha Alhurais</a></li>
+        </ul>
+    </div>
+    <div class="container">
+        <h4>SUMMARY</h4>
+        <hr />
+        <p>Aspiring software engineer with a strong foundation in web development, backend programming, and mobile
+            application development. Experienced in creating innovative solutions, including a mobile reminder system
+            for Alzheimer’s patients. Published researcher with a focus on developing scalable and efficient software.
+            Recently completed a Master’s degree in Computer Science, specializing in advanced software engineering,
+            algorithms, and DevOps practices.</p>
+        <h4>EDUCATION</h4>
+        <hr />
+        <h6>- Arab East Colleges | Saudi Arabia</h6>
+        <small>Master’s degree in Computer Science Software Engineering track | Jan 2022 – June 2024
+            GPA: 3.76 / 4
+        </small>
+       
+        <h6>- Imam Abdulrahman Bin Faisal University | Saudi Arabia</h6>
+        <small>Bachelor’s degree in Management Information Systems | 2015 - 2019
+        </small>
+       
+        <h4>OTHER</h4>
+        <hr />
+        <h6>Publications:</h6>
+        <div class="container">
+        <h6> - Graduation project | An E-Sick Leave Application for Students and Faculty Members by Using DevOps
+            Tools | May 2024
+        </h6>
+        <h6> - Research paper in IRJET (International Journal of Engineering and Technology) | Title: Analyzing and Implementing a Mobile Reminder System for Alzheimer’s Patients | Dec 2018
+        </h6>
+        <h6> - Graduation project | Memory Lane; Built an android application that helps Alzheimer’s Patients stay connected with family members | Nov 2018
+        </h6>
+    </div>
+    <h4>Technical Skills:</h4>
+    <hr />
+    <h6> - Microsoft office</h6>
+    <h4 class="pb-5">Languages: <small>Arabic, English</small></h4>
+    </div>
+    </div> 
+)
+    
+    
+
+ReactDOM.render(element, root)
+
+
+
+
+
+ReactDOM.render(element, root)
